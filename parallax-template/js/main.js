678185da30834c4e4dabe6e696c6d2a17bf5fd26
Auri <aurihrp@hotmail.com>
2019-02-04 19:39:50 -0800

    Recieving responses from API with proper results, moving on to appending data.

---
+++ parallax-template/js/main.js
@@ -0,0 +1,70 @@
+function testPerformers(){
+    //searches for artist AND teams
+    var searchTopic = $("#event_name").val().trim();
+    console.log(searchTopic);
+    var location = $("#location").val().trim();
+    // Replacing spaces with dashes on search, API doesn't register spaces.
+    var searchReadyTopic = searchTopic.replace(/\s/g, "-");
+    var queryURL = 'https://api.seatgeek.com/2/events?performers.slug=' + searchReadyTopic
+     + '&client_id=MTUwOTQwOTh8MTU0ODkwODc0NS43Mw';
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+        console.log(response.events[0].title);
+
+
+
+    });
+}
+
+function testEventType(){
+    var searchTopic = $("#event_name").val().trim();
+    // 
+    var location = $("#location").val().trim();
+    // Replacing spaces with dashes on search, API doesn't register spaces.
+    var searchReadyTopic = searchTopic.replace(/\s/g, "-");
+    var queryURL = 'https://api.seatgeek.com/2/events?taxonomies.name=' + searchReadyTopic
+     + '&client_id=MTUwOTQwOTh8MTU0ODkwODc0NS43Mw';
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+        console.log(response.events[0].title);
+
+
+
+    });
+}
+
+//testGenre can be used for event name search as well.
+
+function testGenre(){
+    var searchTopic = $("#event_name").val().trim();
+    // 
+    var location = $("#location").val().trim();
+    // Replacing spaces with dashes on search, API doesn't register spaces.
+    var searchReadyTopic = searchTopic.replace(/\s/g, "-");
+    var queryURL = 'https://api.seatgeek.com/2/events?q=' + searchReadyTopic
+     + '&client_id=MTUwOTQwOTh8MTU0ODkwODc0NS43Mw';
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+    console.log(response.events);
+
+
+
+});
+
+
+
+}
+
+
