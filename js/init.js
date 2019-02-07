4498ad07811f1b66880a6dffaf297f500b728509
Auri <aurihrp@hotmail.com>
2019-02-06 19:39:18 -0800

    Up to date with amster"

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
